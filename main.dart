fd28ccb477a2599ab8d96bcf3061bf3beb48e556
Sayf <sb24afs@herts.ac.uk>
2025-12-08 01:11:37 +0000

    Update main.dart
    Forgot to add my comments

--- main.dart
+++ main.dart
@@ -11,8 +11,9 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      debugShowCheckedModeBanner: false,  //This makes the program not show the debug banner when testing/running
+      home: LoginPage(),                  //App begins on the login page
     );
   }
 }
+
