da0002702b14e3fce2ff656404933a753804cdfd
Dacres0 <da24aby@herts.ac.uk>
2025-12-10 16:23:38 +0000

    Add Firebase initialization in main function
    Initialize Firebase before running the app.

--- main.dart
+++ main.dart
@@ -1,8 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:unity_main/auth/login_or_register.dart';
 import 'package:unity_main/brightness/light_mode.dart';
 import 'package:flutter/material.dart';
+import 'package:unity_main/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform); 
   runApp(const MainApp());
 }
 
