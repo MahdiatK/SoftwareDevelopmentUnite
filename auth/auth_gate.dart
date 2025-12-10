206f956310a715ac0bd73853a9e02c1c88bc8373
Dacres0 <da24aby@herts.ac.uk>
2025-12-10 17:16:55 +0000

    Add AuthGate widget with placeholder

--- auth/auth_gate.dart
+++ auth/auth_gate.dart
@@ -1,1 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AuthGate extends StatelessWidget {
+  const AuthGate({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
 
