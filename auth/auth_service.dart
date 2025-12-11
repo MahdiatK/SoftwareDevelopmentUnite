505dac9bd05457bd88dd565f168efe111d517d7b
MK <163010589+MahdiatK@users.noreply.github.com>
2025-12-11 17:15:04 +0000

    Integrate Firestore in AuthService for user registration
    Added Firestore integration to save user info upon registration.

--- auth/auth_service.dart
+++ auth/auth_service.dart
@@ -1,8 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 
 class AuthService {
-  //instance of auth 
+  //instance of auth and firestore 
   final FirebaseAuth _auth = FirebaseAuth.instance; 
+  final FirebaseFirestore _firestore =  FirebaseFirestore.instance;
 
   //sign in
   Future<UserCredential> signInWithEmailPassword(String email, String password) async {
@@ -17,7 +20,20 @@ class AuthService {
   //sign up
   Future<UserCredential> registerWithEmailPassword(String email, String password) async {
     try {
-      UserCredential userCredential = await _auth.createUserWithEmailAndPassword(email: email, password: password, );
+      // create user 
+      UserCredential userCredential = 
+          await _auth.createUserWithEmailAndPassword(
+        email: email,
+       password: password,
+      );
+
+      // save user info in separate document
+      _firestore.collection("Users").doc(userCredential.user!.uid).set(
+        {
+          
+        },
+      );
+
       return userCredential;
     } on FirebaseAuthException catch (e) {
       throw Exception(e.code);
