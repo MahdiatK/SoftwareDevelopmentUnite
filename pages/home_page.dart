6b675de88caa055c2c851cee849175d81a8bf557
Dacres0 <da24aby@herts.ac.uk>
2025-12-11 11:40:41 +0000

    Add logout functionality to HomePage

--- pages/home_page.dart
+++ pages/home_page.dart
@@ -1,13 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:unity_main/auth/auth_service.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
+  void logout() async {
+    //get auth service
+    final auth = AuthService();
+    await auth.signOut();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Home")),
-    ); //scaffold
+      appBar: AppBar(
+        title: const Text("Home"),
+        actions: [
+          // logout button
+          IconButton(
+            onPressed: logout, 
+            icon: const Icon(Icons.logout),
+          ),
+        ],
+      ),
+    );
 
   }
 }
