a8659271407c090f1abdc54199974a9f469ac860
Dacres0 <da24aby@herts.ac.uk>
2025-12-12 12:14:47 +0000

    Implement chat message display and input
    Added message display functionality and user input section.

--- pages/chat_page.dart
+++ pages/chat_page.dart
@@ -1,5 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:unity_main/services/auth/auth_service.dart';
+import 'package:unity_main/services/chat/chat_service.dart';
 
 class ChatPage extends StatelessWidget {
   final String receiverEmail;
@@ -30,12 +32,49 @@ class ChatPage extends StatelessWidget {
     }
   }
   
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(receiverEmail)),
+      body: Column(children: [
+        // display all the messages
+        Expanded(child: _buildMessagesList(),
+        ),
+
+        //display the users input
+      ]),
     );
   }
-}
+
+  // build messages list
+  Widget _buildMessagesList() {
+    String SenderID = _authService.currentUser!.uid;
+    return StreamBuilder(
+      stream: _chatService.getMessagesStream(SenderID, receiverID),
+      builder: (context, snapshot) {
+        //error
+        if (snapshot.hasError) {
+          return const Text("Error");
+        }
+
+        //loading
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Text("Loading...");
+        }
+
+        //return list view of messages
+        return ListView(
+          children:
+              snapshot.data!.docs.map((doc) => _buildMessageItem(doc)).toList(),
+          );
+        },
+      );
+    }
+
+    // build individual message item
+    Widget _buildMessageItem(DocumentSnapshot doc) {
+      Map<String, dynamic> data = doc.data() as Map<String, dynamic>;
+
+      return Text(data["message"]);
+    }
+  }
