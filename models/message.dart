6ccd74dd80bb601fcc0f973e45e91226f87e4e4d
Dacres0 <da24aby@herts.ac.uk>
2025-12-12 11:34:36 +0000

    Update message.dart

--- models/message.dart
+++ models/message.dart
@@ -1,1 +1,28 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 
+class Message {
+  final String senderID;
+  final String senderemail;
+  final String receiverID;
+  final String message;
+  final Timestamp timestamp;
+
+  Message({
+    required this.senderID,
+    required this.senderemail,
+    required this.receiverID,
+    required this.message,
+    required this.timestamp,
+  });
+
+  //conmvert to a map
+  Map<String, dynamic> toMap() {
+    return {
+      'senderID': senderID,
+      'senderemail': receiverID,
+      'receiverID': receiverID,
+      'message': message,
+      'timestamp': timestamp,
+    };
+  }
+}
