510275b7396d233a2e5ad86329359efbeb496380
Sayf <sb24afs@herts.ac.uk>
2025-12-13 16:07:09 +0000

    Define dark mode theme using ColorScheme
    Updated dark mode theme to use ColorScheme with surface color.

--- brightness/dark_mode.dart
+++ brightness/dark_mode.dart
@@ -1,1 +1,12 @@
+import 'package:flutter/material.dart';
+
+ThemeData darkMode = ThemeData(
+  colorScheme: ColorScheme.dark(
+    surface: Colors.grey.shade900,   //background is depreciated i.e used surface instead
+    primary: Colors.grey.shade600,
+    secondary: Colors.grey.shade700,
+    tertiary: Colors.grey.shade800,
+    inversePrimary: Colors.grey.shade300,
+  ),
+);
 
