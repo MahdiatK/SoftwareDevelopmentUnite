e4eb7845e2e0468d885c3facd4fe17389280df89
Dacres0 <da24aby@herts.ac.uk>
2025-12-17 11:57:58 +0000

    Create ProfilePage widget with layout
    Added a new ProfilePage widget with basic layout.

--- pages/profile_page.dart
+++ pages/profile_page.dart
@@ -1,1 +1,28 @@
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      appBar: AppBar(
+        title: const Text("Profile"),
+        backgroundColor: Colors.transparent,
+        foregroundColor: Colors.grey,
+        elevation: 0,
+      ),
+      body: Center(
+        child: Text(
+          "Profile Page",
+          style: TextStyle(
+            color: Theme.of(context).colorScheme.primary,
+            fontSize: 20,
+          ),
+        ),
+      ),
+    );
+  }
+}
 
